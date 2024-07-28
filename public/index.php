ef02545dc19a774258159215f848dd78eb68913f
anonyymi <anonyymigithub@gmail.com>
2024-07-28 23:19:48 +0300

    impl very crude sql statement builder

--- public/index.php
+++ public/index.php
@@ -4,12 +4,13 @@ namespace minichan;
 
 use minichan\http;
 use minichan\cache;
+use minichan\db;
 
 require __DIR__ . '/bootstrap.php';
 
+require __ROOT__ . '/php/http/router.php';
 require __ROOT__ . '/php/cache/cache.php';
 require __ROOT__ . '/php/db/db.php';
-require __ROOT__ . '/php/http/router.php';
 
 $cache = new cache\FileCache('main');
 $router = new http\Router();
@@ -47,6 +48,45 @@ $router->add_route(HTTP_GET, '/cache/:key/:val/set', function ($vars) use (&$cac
 $router->add_route(HTTP_GET, '/cache/flush', function ($vars) use (&$cache) {
 	$cache->flush();
 	echo "flushed cache";
+});
+
+$router->add_route(HTTP_GET, '/test/query', function ($vars) {
+	$sql = db\Sql::new()
+		->select([
+			'MAX(sq1.total_posts) AS total_posts',
+			'MAX(sq1.current_posts) AS current_posts',
+			'MAX(sq1.unique_posters) AS unique_posters',
+			'MAX(sq1.imported_posts) AS imported_posts',
+			'MAX(sq1.current_files) AS current_files',
+			'MAX(sq1.active_content) AS active_content',
+		])
+		->from(db\Sql::new()
+			->select([
+				'SUM(sq11.total_posts) AS total_posts',
+				'NULL AS current_posts',
+				'NULL AS unique_posters',
+				'NULL AS imported_posts',
+				'NULL AS current_files',
+				'NULL AS active_content',
+			])
+			->from(db\Sql::new()
+				->select(['board_id AS board_id', 'MAX(post_id) AS total_posts'])
+				->from(db\Sql::new()->literal('posts'))
+			, 'sq11')
+
+			->op('UNION ALL')
+
+			->select([
+				'NULL AS total_posts',
+				'COUNT(*) AS current_posts',
+				'NULL AS unique_posters',
+				'NULL AS imported_posts',
+				'NULL AS current_files',
+				'NULL AS active_content',
+			])
+			->from(db\Sql::new()->literal('posts'))
+		, 'sq1');
+	echo $sql->get();
 });
 
 $match = $router->match_route($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
--- public/php/db/db.php
+++ public/php/db/db.php
@@ -45,3 +45,115 @@ class Connection
     return $this->pdo->rollBack();
   }
 }
+
+// TODO: just a quick sketch for now...
+class Sql
+{
+  private string $raw;
+  private bool $literal;
+
+  private function __construct(string $raw)
+  {
+    $this->raw = $raw;
+    $this->literal = false;
+  }
+
+  public static function new(string $raw = ''): Sql
+  {
+    return new Sql($raw);
+  }
+
+  public function get(): string
+  {
+    return $this->raw;
+  }
+
+  public function literal(string $raw): Sql
+  {
+    $this->raw = $raw;
+    $this->literal = true;
+
+    return $this;
+  }
+
+  public function is_literal(): bool
+  {
+    return $this->literal;
+  }
+
+  public function select(array $cols): Sql
+  {
+    $cols_n = count($cols);
+
+    $this->raw .= "SELECT\n";
+    foreach ($cols as $idx => $col) {
+      $this->raw .= $col;
+      if ($idx !== $cols_n - 1) {
+        $this->raw .= ',';
+      }
+      $this->raw .= "\n";
+    }
+
+    return $this;
+  }
+
+  public function from(Sql $sql, ?string $as = null): Sql
+  {
+    if ($sql->is_literal()) {
+      $this->raw .= "FROM {$sql->get()}";
+    } else {
+      $this->raw .= "FROM ({$sql->get()})";
+    }
+
+    if ($as != null) {
+      $this->raw .= " AS $as";
+    }
+
+    $this->raw .= "\n";
+
+    return $this;
+  }
+
+  public function where(Sql $sql): Sql
+  {
+    if ($sql->is_literal()) {
+      $this->raw .= "WHERE {$sql->get()}\n";
+    } else {
+      $this->raw .= "WHERE ({$sql->get()})\n";
+    }
+
+    return $this;
+  }
+
+  public function conditional(string $lhs, string $op, int|string $rhs): Sql
+  {
+    if (gettype($rhs) === 'string') {
+      $this->raw .= "$lhs $op \'$rhs\'\n";
+    } else {
+      $this->raw .= "$lhs $op $rhs\n";
+    }
+
+    return $this;
+  }
+
+  public function and(): Sql
+  {
+    $this->raw .= "AND\n";
+
+    return $this;
+  }
+
+  public function or(): Sql
+  {
+    $this->raw .= "OR\n";
+
+    return $this;
+  }
+
+  public function op(string $op): Sql
+  {
+    $this->raw .= "$op\n";
+
+    return $this;
+  }
+}
